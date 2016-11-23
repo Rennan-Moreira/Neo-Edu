70a36c3dcc0afb110a3f37c5dbd4c54c3304c988
rennan-moreira <rennanmore@gmail.com>
2016-11-23 18:07:05 -0200

    ok

--- app/src/main/java/com/example/rennan/neoedu/HomeActivity.java
+++ app/src/main/java/com/example/rennan/neoedu/HomeActivity.java
@@ -59,7 +59,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        //getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
@@ -89,8 +89,6 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
